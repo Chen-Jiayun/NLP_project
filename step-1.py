1816f0427ffe54ffab5fb393f2e3948a9793d1bf
MangoRespect <108246430+MangoRespect@users.noreply.github.com>
2023-08-02 21:37:39 +0100

    fix get data bug

--- step-1.py
+++ step-1.py
@@ -1,21 +1,26 @@
 import os
 import json
 
-def get_title(name):
+def get_raw_text(name):
     with open(name, "r", encoding="utf-8") as file:
         data = json.load(file)
     titles = data["metadata"]["title"]
     abstract = data.get("abstract", None)
+    abstract_text = []
+
     print(titles)
     if abstract:
-        print(abstract)
+        for item in abstract:
+            abstract_text.append(item.get("text",None))
+        for item_text in abstract_text:
+            print(item_text)
 
 def process_data(path):
     for root, dirs, files in os.walk(path):
         for file in files:
             if file.endswith(".json"):
                 file_path = os.path.join(root, file)
-                get_title(file_path)
+                get_raw_text(file_path)
 
 # export NLP_DATA_PATH="..."
 
